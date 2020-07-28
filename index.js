84821ae2d6cc79e7e1a193e0eca7f54bf6e8543c
Saleem Saleh <salem1234saleem@gmail.com>
2020-07-28 18:56:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 // Code your solution here
 function findMatching (drivers ,string){
   return drivers.filter( n => {
-  return n  === string;
+  return n  === string || n.toLowerCase() === string.toLowerCase();
 });
+}
 
+function fuzzyMatch (drivers ,string){
+  return drivers.filter( n => {
+  return n.substring(0, string.length)=== string ;
+});
+}
+
+function matchName  (drivers ,string){
+  return drivers.filter( n => {
+  return n.name=== string ;
+});
 }
