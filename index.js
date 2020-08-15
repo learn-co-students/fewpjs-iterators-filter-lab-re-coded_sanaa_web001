fa97b71446a0024ef317905f91b63db2d196754d
Nusiba Ibrahim <ainusiba@gmail.com>
2020-08-15 20:22:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here
+
+const drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "bobby"];
+function findMatching(arr, query) {
+  return arr.filter((n) => {
+    return n.toLowerCase() === query.toLowerCase();
+  });
+}
+console.log(findMatching(drivers, "Bobby"));
+console.log(findMatching(drivers, "Sammy"));
+console.log(findMatching(drivers, "sobby"));
+
+function fuzzyMatch(arr, query) {
+  drivers.length = 0;
+  drivers.push("Bobby", "Sammy", "Sally", "Annette", "Sarah", "bobby");
+  return arr.filter((n) => {
+    return n.startsWith(query);
+  });
+}
+fuzzyMatch(drivers);
+
+function matchName(arr, query) {
+  return arr.filter((n) => {
+    return n.name == query;
+  });
+}
