17fa8e2d616dfebc3198441713a0a2a2590fc848
Osamah Gharad <osamah.gharred@gmail.com>
2020-08-21 00:21:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-function findMatching(drivers,string){
-  drivers.filter(driver =>{
-    return driver ==string;
-  });
+function findMatching(drivers,name){
+ return drivers.filter(driver =>{
+    return driver.toLowerCase() === name.toLowerCase();
+  })
 
+}
+
+function fuzzyMatch(drivers,name){
+  return drivers.filter(driver =>  driver.slice(0,name.length)===name)
+}
+
+function matchName(drivers, name) {
+  return drivers.filter(driver => driver.name == name);
 }
