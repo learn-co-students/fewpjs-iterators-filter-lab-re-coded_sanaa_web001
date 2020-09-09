149537e3900b8f998309abd9e824df15473d1dc4
Marwa Alaghbri <marwaalaghbry@gmail.com>
2020-09-09 23:00:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution here
+  const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+  
+  //const
+
+  function findMatching(drivers ,find)
+  {
+    return drivers.filter(el => el.toLowerCase().indexOf(find.toLowerCase()) !== -1)
+  }
+  
+
+function fuzzyMatch(drivers, letters) {
+
+  return drivers.filter(function (word) {
+     let beginOfletter = word.slice(0, letters.length)
+     return beginOfletter.includes(letters)
+  });
+}
+
+
+function matchName(drivers, name) {
+  return drivers.filter(driverName => {
+    return driverName.name === name
+  })
+}
