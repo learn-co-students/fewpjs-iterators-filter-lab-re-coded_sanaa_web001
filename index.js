2b88c65ec8534e0643db9663bcd8e9dbf85b0fa9
Ahmed Al-Majidi <af774857999@gmail.com>
2020-09-15 14:33:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching (str, name) {
+  return str.filter( (driver) => {
+    return driver.toLowerCase() === name.toLowerCase();
+  });
+}
+function fuzzyMatch (str, firstLetter) {
+  let len = firstLetter.length;
+  let x = (driver) => driver.slice(0, len) === firstLetter
+  return str.filter(x);
+}
+
+function matchName (str, name) {
+  let x = (driver) => driver.name.toLowerCase() === name.toLowerCase()
+  return str.filter(x);
+}
